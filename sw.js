3038417ccf9df4415bd4e5e7e79911f3ff281ceb
butlershop88 <butlershop88@gmail.com>
2025-10-02 19:09:34 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tareas-app-v21'; // Aumenta este número en cada actualización
+const CACHE_NAME = 'tareas-app-v22'; // Aumenta este número en cada actualización
 const urlsToCache = [
     './',
     './index.html',
